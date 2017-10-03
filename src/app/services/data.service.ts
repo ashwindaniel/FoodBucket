1149b07184af5b258a922cc960b7f203afb284ac
ashwindaniel <d.redvirus@gmail.com>
2017-10-03 14:48:07 +0530

    changes

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { TableData } from '../interface/table-data';
+import { AppConfig } from '../interface/app-config';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class DataService {
-  constructor(public http: Http) { }
+  constructor(public http: Http, private appConfig: AppConfig) { }
 
-  getPost(target: string) {
-    return this.http.get('http://localhost:3000/' + target)
+  getFood(target: string) {
+    return this.http.get('http://localhost:3000' + target)
       .map(res => res.json());
   }
   // sendPost(target: string, sendData: TableData) {
